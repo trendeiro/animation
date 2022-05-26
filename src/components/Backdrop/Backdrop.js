1889dffe0dac5845bcabeef7669a611741b02b2e
trendeiro <torendeiro@gmail.com>
2022-05-26 11:21:30 +0100

    animations in react

---
+++ src/components/Backdrop/Backdrop.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Backdrop.css";
+
+const backdrop = (props) => {
+  const cssClass = [
+    "Backdrop",
+    props.show ? "BackdroplOpen" : "BackdropClosed",
+  ];
+  return <div className={cssClass.join(" ")}></div>;
+};
+
+export default backdrop;
